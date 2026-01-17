08ae96c8a17486eac1853a07c2cc0a7c76688c00
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-01-17 16:29:04 +0100

    feat(auth): install ba admin pluging

--- src/server/better-auth/server.ts
+++ src/server/better-auth/server.ts
@@ -1,7 +1,27 @@
-import { headers } from "next/headers";
-import { cache } from "react";
-import { auth } from ".";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import { admin as adminPlugin } from "better-auth/plugins";
+import { env } from "@/env";
+import { db } from "@/server/db";
 
-export const getSession = cache(async () =>
-	auth.api.getSession({ headers: await headers() }),
-);
+export const auth = betterAuth({
+	database: prismaAdapter(db, {
+		provider: "postgresql", // or "sqlite" or "mysql"
+	}),
+	emailAndPassword: {
+		enabled: false,
+	},
+	socialProviders: {
+		microsoft: {
+			clientId: env.MICROSOFT_CLIENT_ID as string,
+			clientSecret: env.MICROSOFT_CLIENT_SECRET as string,
+			tenantId: "6f1276a0-6c96-449b-a10f-1a3d157a8bc4",
+			authority: "https://login.microsoftonline.com",
+			prompt: "select_account",
+		},
+	},
+	plugins: [adminPlugin(), nextCookies()],
+});
+
+export type Session = typeof auth.$Infer.Session;
