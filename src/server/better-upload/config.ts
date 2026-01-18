081e5075757b88a7aead32656e21722a9124d43d
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-01-18 21:09:11 +0100

    feat(upload): init better-upload

---
+++ src/server/better-upload/config.ts
@@ -0,0 +1,18 @@
+import { type Router, route } from "@better-upload/server";
+import { custom } from "@better-upload/server/clients";
+import { env } from "@/env";
+
+const s3 = custom({
+	host: env.STORAGE_HOST,
+	accessKeyId: env.STORAGE_ACCESS_KEY_ID,
+	secretAccessKey: env.STORAGE_ACCESS_KEY,
+	region: "nbg1",
+	secure: true,
+	forcePathStyle: false,
+});
+
+export const router: Router = {
+	client: s3,
+	bucketName: "my-bucket",
+	routes: {},
+};
