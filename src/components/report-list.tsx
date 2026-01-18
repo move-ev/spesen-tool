d30ce7a3bb091a0baf16a5d1be0cb2c0f007901a
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-01-18 16:40:12 +0100

    feat(ui): implement report list

--- src/components/report-list.tsx
+++ src/components/report-list.tsx
@@ -1,9 +1,19 @@
 import { format } from "date-fns";
-import type { Report } from "generated/prisma/client";
+import type { Expense, Report } from "generated/prisma/client";
 import Link from "next/link";
 import type React from "react";
 import { cn } from "@/lib/utils";
-import { Separator } from "./ui/separator";
+import { ReportStatusBadge } from "./report-status-badge";
+import { Badge } from "./ui/badge";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "./ui/card";
+import { Skeleton } from "./ui/skeleton";
 
 export function ReportList({
 	reports,
@@ -11,7 +21,7 @@ export function ReportList({
 	className,
 	...props
 }: React.ComponentProps<"ul"> & {
-	reports: Report[];
+	reports: (Report & { expenses: Expense[] })[];
 
 	/**
 	 * The route to the report details page. `:reportId` will be replaced
@@ -19,36 +29,116 @@ export function ReportList({
 	 */
 	reportRoute: string;
 }) {
+	if (reports.length === 0) {
+		return (
+			<div
+				className={cn(
+					"rounded-lg border border-dashed p-12 text-center",
+					className,
+				)}
+			>
+				<p className="font-medium text-muted-foreground text-sm">
+					Keine Anträge gefunden
+				</p>
+			</div>
+		);
+	}
+
 	return (
-		<ul className={cn("grid gap-2", className)} {...props}>
-			{reports.map((report, index) => (
-				<>
+		<ul
+			className={cn(
+				"grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3",
+				className,
+			)}
+			{...props}
+		>
+			{reports.map((report) => {
+				const reportTotal = report.expenses.reduce(
+					(sum, expense) => sum + Number(expense.amount),
+					0,
+				);
+
+				return (
 					<li
-						className="relative isolate rounded-md px-4 py-2 hover:bg-muted"
+						className="group/list-item relative isolate rounded-lg"
 						key={report.id}
 					>
-						<div>
-							<Link
-								className="group/list-item focus:outline-0"
-								href={reportRoute.replace(":reportId", report.id)}
-							>
-								<span
-									className={cn(
-										"absolute inset-0 z-50 h-full w-full rounded-md transition-colors",
-										"group-focus/list-item:ring-2 group-focus/list-item:ring-ring group-focus/list-item:ring-offset-4",
-									)}
-								/>
-								<p className="font-medium">{report.title}</p>
-							</Link>
-							<p className="text-muted-foreground text-sm">
-								Erstellt am {format(report.createdAt, "dd.MM.yyyy")} um{" "}
-								{format(report.createdAt, "HH:mm")} Uhr
-							</p>
-						</div>
+						<Card className="flex h-full flex-col transition-colors group-hover/list-item:bg-muted">
+							<CardHeader>
+								<div className="flex flex-col flex-wrap items-start justify-between gap-2 sm:flex-row sm:flex-wrap-reverse">
+									<CardTitle className="order-2 sm:order-1">
+										<Link
+											className="group/list-item focus:outline-0"
+											href={reportRoute.replace(":reportId", report.id)}
+										>
+											<span
+												className={cn(
+													"absolute inset-0 z-50 h-full w-full rounded-lg transition-colors",
+													"group-focus/list-item:ring-2 group-focus/list-item:ring-ring group-focus/list-item:ring-offset-4",
+												)}
+											/>
+											{report.title}
+										</Link>
+									</CardTitle>
+									<ReportStatusBadge className="sm:order-2" status={report.status} />
+								</div>
+								<CardDescription className="line-clamp-2">
+									{report.description}
+								</CardDescription>
+							</CardHeader>
+							<CardContent className="flex-1 border-t pt-4">
+								<dl className="grid gap-4">
+									<div className="grid grid-cols-2">
+										<dt className="text-muted-foreground text-sm">Gesamtbetrag</dt>
+										<dd className="font-medium text-foreground text-sm">
+											{reportTotal.toFixed(2)} €
+										</dd>
+									</div>
+									<div className="grid grid-cols-2">
+										<dt className="text-muted-foreground text-sm">Anzahl Ausgaben</dt>
+										<dd className="font-medium text-foreground text-sm">
+											{report.expenses.length === 0
+												? "Keine Ausgaben"
+												: report.expenses.length}
+										</dd>
+									</div>
+								</dl>
+							</CardContent>
+							<CardFooter className="py-2">
+								<span className="text-xs">
+									Erstellt am {format(report.createdAt, "dd.MM.yyyy")} um{" "}
+									{format(report.createdAt, "HH:mm")} Uhr
+								</span>
+							</CardFooter>
+						</Card>
 					</li>
-					{index !== reports.length - 1 && <Separator className={cn("mx-4")} />}
-				</>
-			))}
+				);
+			})}
+		</ul>
+	);
+}
+
+export function ReportListSkeleton({
+	className,
+	...props
+}: React.ComponentProps<"ul">) {
+	return (
+		<ul
+			className={cn(
+				"grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3",
+				className,
+			)}
+			{...props}
+		>
+			<li>
+				<Skeleton className="h-32 w-full" />
+			</li>
+			<li>
+				<Skeleton className="h-32 w-full" />
+			</li>
+			<li>
+				<Skeleton className="h-32 w-full" />
+			</li>
 		</ul>
 	);
 }
